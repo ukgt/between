5d2fc6f6c647a9ce1c90be2d60965647ad42d20a
ukgt <ukgt2019@gmail.com>
2019-04-18 18:04:48 -0400

    working db for users

--- between/routes/api/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// User Model
-const User = require("../../models/Users");
-
-module.exports = router;
---
+++ routes/api/user.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+// User Model
+const User = require("../../models/Users");
+
+// GET route for api/user
+// GET all Users
+//Authentication here???
+router.get('/', (req, res) => {
+     User.find()
+          .then(user => res.json(user))
+     
+});
+
+//POST api/user
+//CReate a Post
+router.post("/", (req, res) => {
+ const newUser = new User({
+      name: req.body.name
+ });
+newUser.save().then(user => res.json(user));
+
+});
+
+// //DELETE api/user/:id
+// //DELETE a User
+// router.delete("/:id", (req, res) => {
+//  User.findById(req.params.id)
+//  .then(item => item.remove().then(() => res.json({success: true})))
+//  .catch(err => res.status(404).json({ success: false}))
+//  });
+
+
+
+module.exports = router;
